0bf76d4b9f0f0987bea70ead308c6297874805d7
thattommyhall <thattommyhall@gmail.com>
2013-10-08 05:55:52 +0100

    Working async

--- src/cljs/async_life/core.cljs
+++ src/cljs/async_life/core.cljs
@@ -15,7 +15,7 @@
 (def context (atom nil))
 (def width (atom nil))
 (def height (atom nil))
-(def cell-size (atom 50))
+(def cell-size (atom 30))
 (def canvas (atom nil))
 
 (defn fill_sq [x y colour]
@@ -46,18 +46,21 @@
 (defn log [msg]
   (.log js/console msg))
 
-(defn cell [x y]
+(defn cell [[x y]]
   (let [new-neighbor (chan)
         input (chan 20)
         neighbors #{}
         initial-state (rand-nth [:dead :alive])
         ]
+    (if (= initial-state :alive)
+      (fill_sq x y alive))
     (go (loop [neighbor-count 0
                state initial-state
                neighbors neighbors]
           (let [[val chan] (alts! [new-neighbor input])]
             (cond (= chan new-neighbor)
                   (do (if (= state :alive)
+                        (<! (timeout 1000))
                         (>! val 1))
                       (recur neighbor-count state (conj neighbors val)))
 
@@ -71,64 +74,49 @@
                         delta (if (= new-state :alive) 1 (- 1))
                         colour (if (= new-state :alive) alive dead)
                         ]
-                    (log (str x " " y " " neighbor-count))
+                    ;; (log (str x " " y " " neighbor-count))
                     (if draw?
                       (do (doseq [n neighbors]
+                            (<! (timeout 1))
                             (>! n delta))
                           (fill_sq x y colour)))
                     (recur neighbor-count new-state neighbors))))))
     [input new-neighbor]))
 
+(defn neighbors [[x y] grid]
+  (filter (comp not nil?)
+          (for [dx [-1 0 1]
+                dy (if (zero? dx)
+                     [-1 1]
+                     [-1 0 1])]
+            (let [x' (+ dx x)
+                  y' (+ dy y)]
+              (get grid [x' y'])))))
+
 (defn draw-loop []
-  (let [[input1 new-neighbor1] (cell 3 3)
-        [input2 new-neighbor2] (cell 4 4)
+  (let [[input1 new-neighbor1] (cell [3 3])
+        [input2 new-neighbor2] (cell [4 4])
+        n1 (chan 20)
+        xys (for [x (range @width)
+                  y (range @height)]
+              [x y])
+        cells (zipmap xys
+                      (map cell xys))
         ]
-    (go (doseq [n [1 1 1 1 -1 -1 -1 -1]]
-          (<! (timeout 500))
-          ;; (log n)
-          (>! input1 n)
-          (>! input2 n)))
+    (doseq [[xy [input _]] cells]
+      (go (doseq [[_ nn] (neighbors xy cells)]
+            (<! (timeout 1))
+            (>! nn input))))
 
-    ;; (go (loop []
-    ;;       (<! out)
-    ;;       (recur)))
-    ;; (go (loop []
-    ;;       (<! out2))
-    ;;       (recur))
+  ))
 
+(defn ^:export init []
+  (reset! canvas (by-id "world"))
+  (reset! context (.getContext @canvas  "2d"))
+  (resized)
+  (doseq [y (range @height)
+          x (range @width)]
+    (fill_sq x y dead)
     )
-    )
-
-
-  ;; (defn draw-loop []
-  ;;   (let [queue (chan)]
-  ;;     (go
-  ;;      (loop []
-  ;;          (<! (timeout 1))
-  ;;          (>! queue [(Math/floor (rand @width)) (Math/floor (rand @height))])
-  ;;          (recur)))
-
-
-  ;;     (go (loop []
-  ;;           (draw! (<! queue))
-  ;;           (recur)))))
-
-
-  ;; (defn glide []
-  ;;   (let [draw (drawer)]
-  ;;     (go (while true
-  ;;           (<! (timeout 1000))
-  ;;           (>! draw [50 50])))))
-
-
-  (defn ^:export init []
-    (reset! canvas (by-id "world"))
-    (reset! context (.getContext @canvas  "2d"))
-    (resized)
-    (doseq [y (range @height)
-            x (range @width)]
-      (fill_sq x y dead)
-      )
-
-    (draw-loop)
-    )
+  (draw-loop)
+  )
